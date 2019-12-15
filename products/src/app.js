c24accfd600865aa25fba1ab6b2dd29102f04ce6
mitsram <mitchell.ramos@gmail.com>
2019-12-16 00:19:16 +1000

    Initial mysql implementation

--- products/src/app.js
+++ products/src/app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mysql = require('mysql')
 
 app.use(bodyParser.json())
+
+const db = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password: 'root'
+})
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL connected.')
+})
 
 app.get("/", (req, res) => {
     res.json({
---
+++ products/src/models/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'root',
+    password: 'root'
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL connected.')
+})
+
+module.exports = connection
